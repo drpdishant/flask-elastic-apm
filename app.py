bc746b1f49f37e53612663fc360b21d60498cbd9
Dishant Pandya <drpdishant@gmail.com>
2020-12-07 18:53:16 +0530

    update

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+from elasticapm.contrib.flask import ElasticAPM
+app = Flask(__name__)
+app.config['ELASTIC_APM'] = {
+          'SERVICE_NAME': 'FlaskApp',
+          'SECRET_TOKEN': '',         
+          'SERVER_URL': 'http://192.168.33.10:8200'
+}
+apm = ElasticAPM(app)
+
+@app.route('/')  
+def index():   
+ return "Hello World!"
+
+@app.errorhandler(404)
+def not_found_error(error):
+ return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+ return render_template('500.html'), 500
+
+@app.route('/divbyzero')
+def divbyzero():
+    num = 2 / 0
+    return "hello world - " + str(num)
+    
+if __name__ == '__main__':   
+ app.run(host='0.0.0.0', port=5000)
